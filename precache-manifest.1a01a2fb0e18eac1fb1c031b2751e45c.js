bb71b41ea78fea5bf6f148f4118bdf7e5517e642
loctary <39388080+Loctary@users.noreply.github.com>
2020-01-26 23:21:00 +0200

    Updates

---
+++ precache-manifest.1a01a2fb0e18eac1fb1c031b2751e45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7615cb796b26eb313e720f7cbb1a3629",
+    "url": "/iwanna/index.html"
+  },
+  {
+    "revision": "09e5aa2986fbceffc22d",
+    "url": "/iwanna/static/css/main.5c33c966.chunk.css"
+  },
+  {
+    "revision": "25146cf9ecfcdfe592b3",
+    "url": "/iwanna/static/js/2.5c5911bd.chunk.js"
+  },
+  {
+    "revision": "acd954dee83108643118e2cdb727a0a6",
+    "url": "/iwanna/static/js/2.5c5911bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09e5aa2986fbceffc22d",
+    "url": "/iwanna/static/js/main.4a3cd12c.chunk.js"
+  },
+  {
+    "revision": "751c01e604fe12a392a9",
+    "url": "/iwanna/static/js/runtime-main.2303f5bb.js"
+  }
+]);
